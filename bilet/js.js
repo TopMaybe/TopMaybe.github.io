47e233377f3b2ce4d1267bba6d0493d91e149d6c
TopMaybe <lbvf_lbvf_1910@bk.ru>
2023-10-02 22:31:14 +0300

    ver 0.0.1
    никакущий сайт

--- bilet/js.js
+++ bilet/js.js
@@ -1,14 +1,40 @@
-var questions = ["Являются ли тротуары и обочины частью дороги?", "На каком расстоянии до неровного участка дороги устанавливается этот знак вне населенного пункта?", "Требования каких знаков из указанных вступают в силу непосредственно в том месте, где они установлены?"]
-var currentQuestion = 0
+const questions = [
+    [`Являются ли тротуары и обочины частью дороги?`,
+        `+Являются`, `Являются только обочины`, `Не являются`],
+    [`На каком расстоянии до неровного участка дороги устанавливается этот знак вне населенного пункта?`,
+        `+150-300 м`, `50-100 м`, `Непосредственно перед неровным участком дороги`],
+    [`Требования каких знаков из указанных вступают в силу непосредственно в том месте, где они установлены?`,
+        `Только Б`, `+А и Б`, `Всех`]];
 
-document.getElementById("buttonAnswer").addEventListener("click", function() {
-	UpdateText()
-});
+let currentQuestion = 0
+let currentRightAnswer = 0
+let ButtonID = "answer-item"
 
-function UpdateText(){
-	if (currentQuestion>2) currentQuestion = 0
-	document.getElementById("buttonAnswer").innerHTML = `${questions[currentQuestion]}`
-	currentQuestion++
+
+
+
+function SetListener() {
+    for (let i = 1; i < questions[currentQuestion].length; i++) {
+        let button = document.getElementById(`${ButtonID+i}`);
+        if(questions[currentQuestion][i].includes("+")) currentRightAnswer = i;
+        button.innerText = `${questions[currentQuestion][i].replace("+", "")}`;
+        button.addEventListener("click", function () {
+            SetAnswer(i)
+        });
+        // console.log(`${currentRightAnswer}\n${questions[currentQuestion][i]}`)
+    }
+    UpdateText();
 }
 
-UpdateText()
+function UpdateText(){
+    if (currentQuestion>2) currentQuestion = 0
+    document.getElementById("question").innerHTML = `${questions[currentQuestion][0]}`
+    currentQuestion++
+}
+function SetAnswer(idAnswer){
+    document.getElementById(`${ButtonID+idAnswer}`).style.backgroundColor = "red";
+    if (idAnswer === currentRightAnswer) document.getElementById(`${ButtonID+idAnswer}`).style.backgroundColor = '#30ff23';
+}
+
+
+SetListener()
