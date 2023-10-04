de021d47c976498f77e8e26bdfb0f5a0b4674fb7
TopMaybe <lbvf_lbvf_1910@bk.ru>
2023-10-05 00:01:02 +0300

    ver0.0.3(еще раз)
    проблемы с утечкой памяти :\

--- bilet/js.js
+++ bilet/js.js
@@ -1,12 +1,21 @@
 const questions = [
-    [`Являются ли тротуары и обочины частью дороги?`,
+    [`Являются ли тротуары и обочины частью дороги?`,                                                                   //  1
         `+Являются`, `Являются только обочины`, `Не являются`],
-    [`На каком расстоянии до неровного участка дороги устанавливается этот знак вне населенного пункта?`,
+    [`На каком расстоянии до неровного участка дороги устанавливается этот знак вне населенного пункта?`,               //  2
         `+150-300 м`, `50-100 м`, `Непосредственно перед неровным участком дороги`],
-    [`Требования каких знаков из указанных вступают в силу непосредственно в том месте, где они установлены?`,
-        `Только Б`, `+А и Б`, `Всех`]];
+    [`Требования каких знаков из указанных вступают в силу непосредственно в том месте, где они установлены?`,          //  3
+        `Только Б`, `+А и Б`, `Всех`],
+    [`Вам разрешено продолжить движение:`,                                                                              //  4
+    `По траекториям Б или В`, `+По траекториям А или В`, `По любой траектории из указанных`],
+    [`Можно ли Вам выполнить обгон при наличии данной разметки?`,                                                       //  5
+    `Можно`,`Можно, если скорость трактора менее 30 км/ч`,`+Нельзя`],
+    [`В каком месте Вам следует остановиться?`,                                                                         //  6
+    `+Перед светофором`,`Перед пересекаемой проезжей частью`,`В любом из перечисленных`],
+    [`На каком расстоянии от транспортного средства должен быть выставлен знак аварийной остановки в данной ситуации?`, //  7
+    `Не менее 10 м`,`Не менее 15 м`,`Не менее 20 м`, `Не менее 30 м`]
+];
 
-let currentQuestion = 0
+let currentQuestion = 1
 let currentRightAnswer = 0
 let QuestionBtnID = "btn_"
 let AnswerBtnID = "answer-item"
@@ -17,9 +26,11 @@ function SetListenerToAnswer(index) {
         SetAnswer(index)
     });
 }
-function SetTextInBtn(index){
-    if (questions[currentQuestion][index].includes("+")) currentRightAnswer = index;
-    document.getElementById(`${AnswerBtnID + index}`).innerText = `${questions[currentQuestion][index].replace("+", "")}`;
+function SetTextInBtn(){
+    for (let index = 1; index < questions[currentQuestion].length; index++) {
+        if (questions[currentQuestion][index].includes("+")) currentRightAnswer = index;
+        document.getElementById(`${AnswerBtnID + index}`).innerText = `${questions[currentQuestion][index].replace("+", "")}`;
+    }
 }
 function ClearColorBtn(){
     //TODO:
@@ -27,18 +38,14 @@ function ClearColorBtn(){
         btn[i-1].style.backgroundColor = "#ddd"
     }
 }
-function SetImage(index){
+function SetImage() {
     let img = document.getElementById("image");
-    fetch(`./img/n17_${index+1}.jpg`)
-        .then(response => {
-            img.src = response.ok ? `./img/n17_${index+1}.jpg` : "./img/no_picture.png";
-        });
+    img.src = `./img/n17_${currentQuestion + 1}.jpg`
 }
 function SetUpButtons(){
     for (let i = 1; i < questions[currentQuestion].length; i++){
         btn[i-1] = document.getElementById(`${AnswerBtnID+i}`)
         SetListenerToAnswer(i)
-        SetTextInBtn(i)
     }
 }
 
@@ -58,11 +65,19 @@ function SetAnswer(idAnswer){
     document.getElementById(`${AnswerBtnID+idAnswer}`).style.backgroundColor = "red";
     if (idAnswer === currentRightAnswer) document.getElementById(`${AnswerBtnID+idAnswer}`).style.backgroundColor = '#30ff23';
 }
+function RemoveClassHide(){
+    let btn = document.getElementsByClassName("hide-btn")
+    for(let i = 0; i < btn.length; i++)
+        btn[i].classList.remove("hide-btn")
+}
 function GoToQuestion(id){
+    if(currentQuestion === id) return
     currentQuestion = id
     UpdateText()
+    RemoveClassHide()
     ClearColorBtn()
-    SetImage(currentQuestion)
+    SetImage()      // TODO: Мне надо переделать функцию присваивания листнеров, они проходят через 1 вопрос в котором <5 кнопок из-за чего все ломается
+    SetTextInBtn()
     for (let i = questions[currentQuestion].length;  i < 6; i++)
     {
         btn[i] = document.getElementById(`${AnswerBtnID+i}`)
@@ -74,7 +89,9 @@ function CheckAnswers(){
     // Тут должна быть проверка ошибок в ответах
 }
 
-
+document.getElementById("image").addEventListener("error", function (){
+    document.getElementById("image").src = "./img/no_picture.png"
+})
 SetListenerToQuestion()
 SetUpButtons()
-GoToQuestion(currentQuestion)
+GoToQuestion(currentQuestion-1)
